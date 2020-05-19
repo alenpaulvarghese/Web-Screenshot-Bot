6c5d47f8341c8f219b769a1cef9806a81a73a602
alenpaul2001 <alenpaul2001@gmail.com>
2020-05-19 17:12:39 +0530

    added creds.py

---
+++ creds.py
@@ -0,0 +1,5 @@
+import os 
+class my():
+    BOT_TOKEN = os.environ["BOT_TOKEN"]    # from @botfather
+    API_ID = int(os.environ["API_ID"])     # from https://my.telegram.org/apps
+    API_HASH = os.environ["API_HASH"]      # from https://my.telegram.org/apps

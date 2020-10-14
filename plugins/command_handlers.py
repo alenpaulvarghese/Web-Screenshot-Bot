02d9c2947156c7fab07708b9d717fcd797ebcad0
alenpaul2001 <alenpaul2001@gmail.com>
2020-10-14 23:40:52 +0530

    minor changes in command_handler.py

--- plugins/command_handlers.py
+++ plugins/command_handlers.py
@@ -1,6 +1,7 @@
 from pyrogram.types import (
     InlineKeyboardButton,
-    InlineKeyboardMarkup
+    InlineKeyboardMarkup,
+    Message
 )
 from pyrogram import (
     Client,
@@ -12,7 +13,7 @@ import os
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(10)
 
-blacklist = ['drive.google.com', 'tor.checker.in']
+BLACKLIST = ['drive.google.com', 'tor.checker.in', 'youtube.com', 'youtu.be']
 HOME = InlineKeyboardMarkup([
             [InlineKeyboardButton(text='Format - PDF', callback_data='format')],
             [InlineKeyboardButton(text='Page - Full', callback_data="page")],
@@ -21,41 +22,40 @@ HOME = InlineKeyboardMarkup([
             [InlineKeyboardButton(text='▫️ start render ▫️', callback_data="render")],
             [InlineKeyboardButton(text='cancel', callback_data="cancel")]
                             ])
-format_for_logging = "Request from {name} aka @{user}\n\nQuery : {link}\n\nSettings Used : \n {settings}"
 
 
 @Client.on_message(filters.command(["start"]))
-async def start(client, message):
+async def start(_: Client, message: Message) -> None:
     LOGGER.debug(f"USED_CMD --> /start command >> @{message.from_user.username}")
-    await client.send_message(
-        chat_id=message.chat.id,
-        text="Please Send Any Link",
-        reply_to_message_id=message.message_id
+    await message.reply_text(
+        f"<b>Hi {message.from_user.first_name} 👋\n"
+        "I can render website of a given link to either PDF or PNG/JPEG</b>",
+        quote=True,
+        reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton("❓ About", callback_data="about_cb")
+            ]
+        ])
     )
 
 
-@Client.on_message(filters.command(["feedback"]))
-async def feedback(client, message):
-    LOGGER.debug(f"USED_CMD --> /feedback command >> @{message.from_user.username}")
-    await client.send_message(
-        chat_id=message.chat.id,
-        text="for suggetions and feedbacks contact @STARKTM1",
-        reply_to_message_id=message.message_id
-    )
-
-
-@Client.on_message(filters.command(["notworking"]))
-async def notworking(client, message):
-    LOGGER.debug(f"USED_CMD --> /notworking command >> @{message.from_user.username}")
-    await client.send_message(
-        chat_id=message.chat.id,
-        text="Make sure Your Request has http or https prefix",
-        reply_to_message_id=message.message_id
+@Client.on_message(filters.command(["about", 'feedback']))
+async def feedback(_: Client, message: Message) -> None:
+    LOGGER.debug(f"USED_CMD --> /about command >> @{message.from_user.username}")
+    await message.reply_text(
+        text="This project is open ❤️ source",
+        reply_markup=InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("👨🏻‍🦯 Source", url="https://github.com/alenpaul2001/Web-Screenshot-Bot"),
+                InlineKeyboardButton("❓ Bug Report", url="https://github.com/alenpaul2001/Web-Screenshot-Bot/issues")],
+            [InlineKeyboardButton(
+                "🌃 Profile Icon Credit",
+                url="https://www.goodfon.com/wallpaper/art-vector-background-illustration-minimalism-angga-tanta-12.html")]
+            ])
     )
 
 
 @Client.on_message(filters.command(["delete"]) & filters.private)
-async def deleter_(client, message):
+async def delete(_: Client, message: Message) -> None:
     LOGGER.debug(f"USED_CMD --> /delete command >> @{message.from_user.username}")
     try:
         sudo_user = int(os.environ["SUDO_USER"])
@@ -71,9 +71,8 @@ async def deleter_(client, message):
                     writer.write(str(root)+'\n\n'+str(dirs)+'\n\n'+str(files))
             if os.path.isfile('walk.txt'):
                 LOGGER.debug('DEL__CMD --> status pending >> sending file')
-                await client.send_document(
+                await message.reply_document(
                     document='walk.txt',
-                    chat_id=message.chat.id
                 )
                 await random_message.delete()
                 os.remove('walk.txt')
@@ -90,7 +89,7 @@ async def deleter_(client, message):
 
 
 @Client.on_message(filters.command(["report"]) & filters.private)
-async def delete(client, message):
+async def report(client: Client, message: Message) -> None:
     LOGGER.debug(f"USED_CMD --> /report command >> @{message.from_user.username}")
     try:
         sudo_user = int(os.environ["SUDO_USER"])
@@ -115,7 +114,7 @@ async def delete(client, message):
 
 
 @Client.on_message(filters.command(['debug', 'log']) & filters.private)
-async def send_log(client, message):
+async def send_log(_: Client, message: Message) -> None:
     LOGGER.debug(f"USED_CMD --> /debug command >> @{message.from_user.username}")
     try:
         sudo_user = int(os.environ["SUDO_USER"])
@@ -125,7 +124,7 @@ async def send_log(client, message):
         LOGGER.debug('DEL__CMD --> status failed >> user not a sudo')
         return
     if os.path.exists('debug.log'):
-        await client.send_document(
+        await message.reply_document(
             sudo_user,
             'debug.log'
         )

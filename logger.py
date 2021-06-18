7fe26c85e90f3d66398b3ba31bea65f984acff75
alenpaul2001 <alenpaul2001@gmail.com>
2021-06-19 00:04:42 +0530

    added file handler for logging

--- logger.py
+++ logger.py
@@ -6,6 +6,7 @@ import logging
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+    handlers=[logging.FileHandler("debug.log"), logging.StreamHandler()],
     datefmt="[%Y-%m-%d %H:%M:%S %z]",
     force=True,
 )
